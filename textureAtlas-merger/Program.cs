9bb8b86bb83812f8611cff0045c57bda70c189a5
Alex Chirila <alexchirila87@gmail.com>
2021-07-28 00:26:26 +0300

    Preliminary implementation

--- textureAtlas-merger/Program.cs
+++ textureAtlas-merger/Program.cs
@@ -6,12 +6,15 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 using Newtonsoft.Json;
 
+/// dotnet run -- 4 ..\output ..\croplist.json
+
 namespace csharp {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine(string.Join(' ', args));
             if (args.Length < 3) {
-                Console.WriteLine("Invalid arguments. Please the scale multiplier, followed by both the texture .png file and the parameters .json file");
+                Console.WriteLine("Invalid arguments. Please input the scale multiplier, followed by the folder containing the output of textureAtlas-slicer (all the .png files) and the parameters .json file");
+                Console.WriteLine("Example arguments: 4 ..\\output ..\\croplist.json");
                 return;
             }
 
@@ -21,18 +24,27 @@ namespace csharp {
                 return;
             }
 
-            var texture = Image.Load(args[1]);
+            var texturePath = args[1] + "\\slicedImage.png";
+            var texture = Image.Load(texturePath);
             
             var parametersString = File.ReadAllText(args[2]);
             var parameters = JsonConvert.DeserializeObject<Rectangle[]>(parametersString);
             
-            // System.IO.Directory.CreateDirectory("output");
-            // foreach (var parameter in parameters) {
-            //     Console.WriteLine(parameter.ToString());
-            //     var croppedImage = texture.Clone(x => x.Crop(parameter));
-            //     var resultPath = ".\\output\\" + GetHashString(parameter) + ".png";
-            //     croppedImage.SaveAsPng(resultPath);
-            // }
+            foreach (var parameter in parameters) {
+                var upscaledImagePath = args[1] + "\\" + GetSimpleFilename(parameter) + ".png";
+                var upscaledImage = Image.Load(upscaledImagePath);
+                var upscaledImageLocation = new Point(multiplier * parameter.X, multiplier * parameter.Y);
+                texture.Mutate(context => context.DrawImage(
+                    upscaledImage,
+                    upscaledImageLocation,
+                    SixLabors.ImageSharp.PixelFormats.PixelColorBlendingMode.Normal,
+                    SixLabors.ImageSharp.PixelFormats.PixelAlphaCompositionMode.SrcOver,
+                    1.0f));
+            }
+            texture.SaveAsPng(args[1] + "\\finalImage.png");
+        }
+        public static string GetSimpleFilename(Rectangle rectangle) { 
+            return rectangle.X.ToString() + "-" + rectangle.Y.ToString() + "-" + rectangle.Width.ToString() + "-" + rectangle.Height.ToString();
         }
 
         public static string GetHashString(Rectangle rectangle) {
--- textureAtlas-slicer/Program.cs
+++ textureAtlas-slicer/Program.cs
@@ -3,8 +3,11 @@ using System.Security.Cryptography;
 using System.IO;
 using System.Text;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using Newtonsoft.Json;
+
+/// dotnet run -- ..\tex1_512x256_B20814E2D6573DFE_0.png ..\croplist.json
 
 namespace csharp {
     class Program {
@@ -12,24 +15,34 @@ namespace csharp {
             Console.WriteLine(string.Join(' ', args));
             if (args.Length < 2) {
                 Console.WriteLine("Invalid arguments. Please specify both the texture .png file and the parameters .json file");
+                Console.WriteLine("Example arguments: ..\\tex1_512x256_B20814E2D6573DFE_0.png ..\\croplist.json");
                 return;
             }
 
             var texture = Image.Load(args[0]);
-            Console.WriteLine(texture.ToString());
-            
             var parametersString = File.ReadAllText(args[1]);
             var parameters = JsonConvert.DeserializeObject<Rectangle[]>(parametersString);
-            Console.WriteLine(parameters.Length);
             
-            System.IO.Directory.CreateDirectory("output");
+            System.IO.Directory.CreateDirectory("..\\output");
             foreach (var parameter in parameters) {
                 Console.WriteLine(parameter.ToString());
-                var croppedImage = texture.Clone(x => x.Crop(parameter));
-                var resultPath = ".\\output\\" + GetHashString(parameter) + ".png";
+                var croppedImage = texture.Clone(context => context.Crop(parameter));
+                var resultPath = "..\\output\\" + GetSimpleFilename(parameter) + ".png";
+                var emptyImage = new Image<Rgba32>(parameter.Width, parameter.Height);
+                var location = new Point(parameter.X, parameter.Y);
+                texture.Mutate(context => context.DrawImage(
+                    emptyImage,
+                    location,
+                    SixLabors.ImageSharp.PixelFormats.PixelColorBlendingMode.Normal,
+                    SixLabors.ImageSharp.PixelFormats.PixelAlphaCompositionMode.Clear,
+                    1.0f));
                 croppedImage.SaveAsPng(resultPath);
             }
-            return;
+            texture.SaveAsPng("..\\output\\slicedImage.png");
+        }
+
+        public static string GetSimpleFilename(Rectangle rectangle) { 
+            return rectangle.X.ToString() + "-" + rectangle.Y.ToString() + "-" + rectangle.Width.ToString() + "-" + rectangle.Height.ToString();
         }
 
         public static string GetHashString(Rectangle rectangle) {
